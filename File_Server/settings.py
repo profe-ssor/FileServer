7b28d480235bc6d6548fa0f667581faa4cb79aae
profe-ssor <kyerematengcollins93@gmail.com>
2024-07-02 09:11:06 -0700

    URL Update

--- File_Server/settings.py
+++ File_Server/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-nfb)6xloyo*g9=*qhc$$y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True')=="True"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "file-server.onrender.com"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://file-server-xsnl.onrender.com']
 
 
 # Application definition
